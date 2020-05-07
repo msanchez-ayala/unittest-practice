80586d1c1cd3b66b8e44f68a79d4e8226c06f23b
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-05-07 16:22:37 -0400

    testing a class

---
+++ employee.py
@@ -0,0 +1,44 @@
+"""
+This is code adapted from Corey Schafer's unittesting YouTube video.
+"""
+
+import requests
+
+class Employee:
+    """
+    A sample employee class
+    """
+
+    raise_amt = 1.05
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    @fullname.setter
+    def fullname(self, name):
+        self.first, self.last = name.split(' ')
+
+    @fullname.deleter
+    def fullname(self):
+        self.first = None
+        self.last = None
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response!'
