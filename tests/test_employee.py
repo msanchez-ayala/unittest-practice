79dde0ae411954acca9bc020e097bea2354db6aa
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-05-07 20:38:17 -0400

    move around files

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -1,25 +1,45 @@
+"""
+This is code adapted from Corey Schafer's unittesting YouTube video.
+"""
+
 import unittest
 from unittest.mock import patch
-from employee import Employee
+from src.employee import Employee
 
 
 class TestEmployee(unittest.TestCase):
 
+    ### SET UP / TEAR DOWN ###
+
     @classmethod
     def setUpClass(cls):
-        print('setupClass')
+        """
+        Any code we want to run just once upfront for entire set of tests.
+        """
+        print('setupClass ')
 
     @classmethod
     def tearDownClass(cls):
+        """
+        Any code we want to run just once upon finishing entire set of tests.
+        """
         print('teardownClass')
 
     def setUp(self):
+        """
+        Any code we want to run at the beginning of each test.
+        """
         print('setUp')
         self.emp_1 = Employee('Corey', 'Schafer', 50000)
         self.emp_2 = Employee('Sue', 'Smith', 60000)
 
     def tearDown(self):
+        """
+        Any code we want to run at the end of each test.
+        """
         print('tearDown\n')
+
+    ### TESTS ###
 
     def test_email(self):
         print('test_email')
@@ -56,8 +76,10 @@ class TestEmployee(unittest.TestCase):
         print('test_fullname_deleter')
 
         del self.emp_1.fullname
+        del self.emp_2.fullname
 
         self.assertEqual(self.emp_1.fullname, 'None None') 
+        self.assertEqual(self.emp_2.fullname, 'None None') 
 
     def test_apply_raise(self):
         print('test_apply_raise')
@@ -69,13 +91,24 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(self.emp_2.pay, 63000)
 
     def test_monthly_schedule(self):
-        with patch('employee.requests.get') as mocked_get:
+        """
+        Because this function depends on output of a get request, we don't want
+        the test to fail if the website is down (or any reason other than 
+        our code being broken). Thus, we use the patch function as a context
+        manager to generate a mocked result from the get request.
+        """
+        with patch('src.employee.requests.get') as mocked_get:
+
+            ### TRUE CASE ###
+
             mocked_get.return_value.ok = True
             mocked_get.return_value.text = 'Success'
 
             schedule = self.emp_1.monthly_schedule('May')
             mocked_get.assert_called_with('http://company.com/Schafer/May')
             self.assertEqual(schedule, 'Success')
+
+            ### FALSE CASE ###
 
             mocked_get.return_value.ok = False
 
